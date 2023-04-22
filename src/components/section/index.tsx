4c27bdeddeb2ebe49d1638629d05703e35694815
Gabriel Cardoso <gabrielcardosodeveloper@gmail.com>
2023-04-21 23:52:09 -0300

    feat(Create Section Component): :sparkles:

---
+++ src/components/section/index.tsx
@@ -0,0 +1,27 @@
+import { chakra, shouldForwardProp } from '@chakra-ui/react'
+import { motion } from 'framer-motion'
+import { ReactNode } from 'react'
+
+interface SectionProps {
+  children: ReactNode
+  delay: string
+}
+
+const StyledDiv = chakra(motion.div, {
+  shouldForwardProp: prop => {
+    return shouldForwardProp(prop) || prop === 'transition'
+  }
+})
+
+export default function Section({ children, delay = '0' }: SectionProps) {
+  return (
+    <StyledDiv
+      initial={{ y: 10, opacity: 0 }}
+      animate={{ y: 0, opacity: 1 }}
+      transition={{ duration: '0.8', delay }}
+      mb={6}
+    >
+      {children}
+    </StyledDiv>
+  )
+}
