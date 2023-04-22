79a431cc451407af5dfcdc891f3bc3acdc909671
Gabriel Cardoso <gabrielcardosodeveloper@gmail.com>
2023-04-21 23:51:39 -0300

    feat(Add a animation to ThemeToggleButton component): :sparkles:

--- src/components/layouts/components/Navbar/components/ThemeToggleButton/index.tsx
+++ src/components/layouts/components/Navbar/components/ThemeToggleButton/index.tsx
@@ -1,16 +1,28 @@
 import { IconButton, useColorMode, useColorModeValue } from '@chakra-ui/react'
 import { Moon, Sun } from '@phosphor-icons/react'
+import { AnimatePresence, motion } from 'framer-motion'
 
 export default function ThemeToggleButton() {
   const { toggleColorMode } = useColorMode()
 
   return (
-    <IconButton
-      mr={2}
-      aria-label='toggle theme'
-      colorScheme={useColorModeValue('purple', 'orange')}
-      icon={useColorModeValue(<Moon />, <Sun />)}
-      onClick={toggleColorMode}
-    />
+    <AnimatePresence mode='wait' initial={false} >
+      <motion.div
+        style={{ display: 'inline-block' }}
+        key={useColorModeValue('light', 'dark')}
+        initial={{ y: -20, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+        exit={{ y: 20, opacity: 0 }}
+        transition={{ duration: '0.2' }}
+      >
+        <IconButton
+          mr={2}
+          aria-label='toggle theme'
+          colorScheme={useColorModeValue('purple', 'orange')}
+          icon={useColorModeValue(<Moon />, <Sun />)}
+          onClick={toggleColorMode}
+        />
+      </motion.div>
+    </AnimatePresence>
   )
 }
