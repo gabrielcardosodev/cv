3858947d1d9f06dd2ffde7a68c318114376c1a1d
Gabriel Cardoso <gabrielcardosodeveloper@gmail.com>
2023-04-22 20:49:09 -0300

    feat(Implement page transition animation in Home and Works page's): :sparkles:

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,104 +4,101 @@ import { Section } from '@/components/section'
 import { Paragraph } from '@/components/paragraph'
 import { CaretRight } from '@phosphor-icons/react'
 import { BioYear, SectionBio } from '@/components/bio'
-import Head from 'next/head'
+import { PageTransition } from '@/components/PageTransition'
 
 export default function Home() {
   return (
-    <Container>
-      <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <title>Gabriel Cardoso - Homepage</title>
-      </Head>
-
-      <Box
-        borderRadius='lg'
-        bg={useColorModeValue('whiteAlpha.500', 'whiteAlpha.200')}
-        p={3}
-        mt={2}
-        mb={6}
-        textAlign="center"
-      >
-        Hello, I&apos;m a frontend developer based in Rio de Janeiro!
-      </Box>
-
-      <Box display={{ md: "flex" }}>
-        <Box flexGrow={1}>
-          <Heading>
-            Gabriel Cardoso
-          </Heading>
-
-          <p>Frontend Developer</p>
-        </Box>
-
+    <PageTransition title='Home'>
+      <Container>
         <Box
-          flexShrink={0}
-          mt={{ base: 4, md: 0 }}
-          ml={{ md: 6 }}
+          borderRadius='lg'
+          bg={useColorModeValue('whiteAlpha.500', 'whiteAlpha.200')}
+          p={3}
+          mt={2}
+          mb={6}
           textAlign="center"
         >
-          <Image
-            borderColor="whiteAlpha.800"
-            borderWidth={2}
-            borderStyle="solid"
-            maxW="100px"
-            display='inline-block'
-            borderRadius="full"
-            src='/images/gabriel.jpg'
-            alt='my profile image'
-          />
+          Hello, I&apos;m a frontend developer based in Rio de Janeiro!
         </Box>
-      </Box>
 
-      <Section delay='0.1'>
-        <Box>
-          <Heading variant="section-title">
-            Work
-          </Heading>
+        <Box display={{ md: "flex" }}>
+          <Box flexGrow={1}>
+            <Heading>
+              Gabriel Cardoso
+            </Heading>
 
-          <Paragraph>Gabriel is a front-end developer based in Rio de Janeiro with a passion for building digital stuff he wants. He currently works as a front-end developer at {' '}
-            <Link fontWeight='bold' href='/works/lifevet' as={NextLink}>
-              LifeVet
-            </Link>
-            .
+            <p>Frontend Developer</p>
+          </Box>
+
+          <Box
+            flexShrink={0}
+            mt={{ base: 4, md: 0 }}
+            ml={{ md: 6 }}
+            textAlign="center"
+          >
+            <Image
+              borderColor="whiteAlpha.800"
+              borderWidth={2}
+              borderStyle="solid"
+              maxW="100px"
+              display='inline-block'
+              borderRadius="full"
+              src='/images/gabriel.jpg'
+              alt='my profile image'
+            />
+          </Box>
+        </Box>
+
+        <Section delay='0.1'>
+          <Box>
+            <Heading variant="section-title">
+              Work
+            </Heading>
+
+            <Paragraph>Gabriel is a front-end developer based in Rio de Janeiro with a passion for building digital stuff he wants. He currently works as a front-end developer at {' '}
+              <Link fontWeight='bold' href='/works/lifevet' as={NextLink}>
+                LifeVet
+              </Link>
+              .
+            </Paragraph>
+          </Box>
+
+          <Box textAlign="center" my={4}>
+            <NextLink href='/works'>
+              <Button rightIcon={<CaretRight />} colorScheme={useColorModeValue('purple', 'orange')}>
+                My portfolio
+              </Button>
+            </NextLink>
+          </Box>
+        </Section>
+
+        <Section delay='0.1'>
+          <Heading variant="section-title">Bio</Heading>
+
+          <SectionBio>
+            <BioYear>1999</BioYear>
+            Born in Rio de Janeiro, Brazil.
+          </SectionBio>
+
+          <SectionBio>
+            <BioYear>2021</BioYear>
+            Joined Rocketseat programming school to study ReactJS.
+          </SectionBio>
+
+          <SectionBio>
+            <BioYear>2022 to present</BioYear>
+            Works as front-end developer at LifeVet.
+          </SectionBio>
+        </Section>
+
+        <Section delay='0.1'>
+          <Heading variant="section-title">I love</Heading>
+
+          <Paragraph>
+            Music, Anime, Manga, Films, League Of Legends, My dog Apolo, Study
           </Paragraph>
-        </Box>
-
-        <Box textAlign="center" my={4}>
-          <NextLink href='/works'>
-            <Button rightIcon={<CaretRight />} colorScheme={useColorModeValue('purple', 'orange')}>
-              My portfolio
-            </Button>
-          </NextLink>
-        </Box>
-      </Section>
-
-      <Section delay='0.1'>
-        <Heading variant="section-title">Bio</Heading>
-
-        <SectionBio>
-          <BioYear>1999</BioYear>
-          Born in Rio de Janeiro, Brazil.
-        </SectionBio>
-
-        <SectionBio>
-          <BioYear>2021</BioYear>
-          Joined Rocketseat programming school to study ReactJS.
-        </SectionBio>
-
-        <SectionBio>
-          <BioYear>2022 to present</BioYear>
-          Works as front-end developer at LifeVet.
-        </SectionBio>
-      </Section>
-
-      <Section delay='0.1'>
-        <Heading variant="section-title">I love</Heading>
-
-        <Paragraph>
-          Music, Anime, Manga, Films, League Of Legends, My dog Apolo, Study
-        </Paragraph>
-      </Section>
-    </Container >
+        </Section>
+      </Container>
+    </PageTransition>
   )
 }
--- src/pages/works/index.tsx
+++ src/pages/works/index.tsx
@@ -1,7 +1,7 @@
 import { WorkGridItem } from "@/components/grid/WorkGridItem";
 import NextLink from 'next/link'
 import { Section } from "@/components/section";
-import { Container, Divider, Heading, SimpleGrid, Link, Button, Text } from "@chakra-ui/react";
+import { Container, Divider, Heading, SimpleGrid, Link, Button, Text, useColorModeValue } from "@chakra-ui/react";
 import Head from "next/head";
 
 import todoThumbnail from '../../../public/images/todoApp.png'
@@ -9,71 +9,67 @@ import letMeAskThumbnail from '../../../public/images/letMeAskApp.png'
 import grapesFinancesThumbnail from '../../../public/images/grapesFinacesApp.png'
 import pomodoroThumbnail from '../../../public/images/pomodoroApp.png'
 import { GithubLogo } from "@phosphor-icons/react";
+import { PageTransition } from "@/components/PageTransition";
 
 export default function Works() {
   return (
-    <Container>
-      <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <title>Gabriel Cardoso - Works</title>
-      </Head>
+    <PageTransition title="Works">
+      <Container>
+        <Heading as="h3" fontSize={20} mb={4}>
+          Works
+        </Heading>
 
-      <Heading as="h3" fontSize={20} mb={4}>
-        Works
-      </Heading>
+        <SimpleGrid columns={[1, 1, 2]} gap={6}>
+          <Section delay="0.3">
+            <WorkGridItem
+              id="todo"
+              title="Todo"
+              thumbnail={todoThumbnail}
+            >
+              To do app to organizing your day more efficiently.
+            </WorkGridItem>
+          </Section>
 
-      <SimpleGrid columns={[1, 1, 2]} gap={6}>
-        <Section delay="0.3">
-          <WorkGridItem
-            id="todo"
-            title="Todo"
-            thumbnail={todoThumbnail}
-          >
-            To do app to organizing your day more efficiently.
-          </WorkGridItem>
-        </Section>
+          <Section delay="0.3">
+            <WorkGridItem
+              id="letmeask"
+              title="Let me ask"
+              thumbnail={letMeAskThumbnail}
+            >
+              App developed for streamers to better organize their viewers&#39; questions.
+            </WorkGridItem>
+          </Section>
 
-        <Section delay="0.3">
-          <WorkGridItem
-            id="letmeask"
-            title="Let me ask"
-            thumbnail={letMeAskThumbnail}
-          >
-            App developed for streamers to better organize their viewers&#39; questions.
-          </WorkGridItem>
-        </Section>
+          <Section delay="0.3">
+            <WorkGridItem
+              id="grapesfinances"
+              title="Grapes Finances"
+              thumbnail={grapesFinancesThumbnail}
+            >
+              App created to organize and control your expenses.
+            </WorkGridItem>
+          </Section>
 
-        <Section delay="0.3">
-          <WorkGridItem
-            id="grapesfinances"
-            title="Grapes Finances"
-            thumbnail={grapesFinancesThumbnail}
-          >
-            App created to organize and control your expenses.
-          </WorkGridItem>
-        </Section>
+          <Section delay="0.3">
+            <WorkGridItem
+              id="pomodoro"
+              title="Pomodoro"
+              thumbnail={pomodoroThumbnail}
+            >
+              A pomodoro timer.
+            </WorkGridItem>
+          </Section>
+        </SimpleGrid>
 
-        <Section delay="0.3">
-          <WorkGridItem
-            id="pomodoro"
-            title="Pomodoro"
-            thumbnail={pomodoroThumbnail}
-          >
-            A pomodoro timer.
-          </WorkGridItem>
-        </Section>
-      </SimpleGrid>
+        <Divider borderColor="gray" mb={10} />
 
-      <Divider mb={10} />
-
-      <Button
-        w="100%"
-        h={50}
-      >
-        <Link
+        <Button
           as={NextLink}
           href="https://github.com/gabrielcardosodev"
           target="_blank"
+          w="100%"
+          h={50}
+          colorScheme={useColorModeValue('purple', 'orange')}
         >
           <Text
             display="flex"
@@ -82,8 +78,8 @@ export default function Works() {
           >
             View more in my github <GithubLogo weight="fill" />
           </Text>
-        </Link>
-      </Button>
-    </Container >
+        </Button>
+      </Container>
+    </PageTransition >
   )
 }
