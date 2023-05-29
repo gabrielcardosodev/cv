94d68dca8b04a974188bb7b1797fcda316145400
Gabriel Cardoso <gabrielcardosodeveloper@gmail.com>
2023-05-29 03:02:43 -0300

    feat(created collaborations mock): :sparkles:

---
+++ src/consts/collaborations.ts
@@ -0,0 +1,20 @@
+import lifevetThumbnail from '../../public/images/lifevetThumbnail.png'
+import rangelThumbnail from '../../public/images/rangelThumbnail.png'
+
+const collaborations = [
+  {
+    id: 'lifevet-client-app',
+    title: 'Lifevet',
+    thumbnail: lifevetThumbnail.src,
+    description: 'App created to client access.'
+  },
+
+  {
+    id: 'rangel-imoveis',
+    title: 'Rangel Imovéis',
+    thumbnail: rangelThumbnail.src,
+    description: 'A website for a real estate'
+  },
+]
+
+export default collaborations
